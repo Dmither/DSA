f20b8c84cd1d222676624ef01441b1fa1dcea431
Dmither <mr.d.graumann@gmail.com>
2023-12-28 12:31:07 +0200

    update

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-arr = [1, 2, 0, 0, 0, 3, 6]
-print(arr)
-
-def zeroes_to_end(arr):
-    j = 0
-    for i in range(len(arr)):
-        if arr[i] != 0:
-         arr[i], arr[j] = arr[j], arr[i]
-         j += 1;
-    return arr
-
-print(zeroes_to_end(arr))
-
+for i in range(1000):
+  if (-i + 2) == 0:
+    continue
+  if (((3 * -i) + 31) % (-i + 2) == 0):
+    print(i)
